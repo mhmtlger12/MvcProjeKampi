8e102e9f5167726d37a516dfbb43ee3be8d3ceb7
Mehmet Güler <mehmet.guler@dpu.edu.tr>
2024-05-28 15:03:02 +0300

    son

--- MvcProjeKampi/Controllers/HeadingController.cs
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +12,44 @@ namespace MvcProjeKampi.Controllers
     public class HeadingController : Controller
     {
         // GET: Heading
-        HeadingManager hm= new HeadingManager( new EfHeadingsDal());
+        HeadingManager hm = new HeadingManager(new EfHeadingsDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        WriterManager wm = new WriterManager(new EfWriterDal());
         public ActionResult Index()
         {
             var headingvalues = hm.GetList();
             return View(headingvalues);
+        }
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            //Burada kategori ıd ile kategorı adını dropdownliste çekiyoruz
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+
+                                                  }).ToList();
+           
+            //Burada Yazar ıd ile Yazar adını dropdownliste çekiyoruz
+            List<SelectListItem> valuewriter = (from x in wm.GetList()
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.WriterName + " " + x.WriterSurName,
+                                                    Value = x.WriterID.ToString()
+
+                                                }).ToList();
+            ViewBag.vlc = valuecategory;
+            ViewBag.vlw=valuewriter;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            hm.HeadingAdd(p);
+            return RedirectToAction("Index");
         }
     }
 }
